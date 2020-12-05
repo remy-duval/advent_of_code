017acb6dcd297eba3ce2d0428a3c059db7c43274
Remy Duval <remy.duval@macq.eu>
2020-12-05 13:08:18 +0100

    feat: add solution for year 2020 day 05

--- src/year2020/mod.rs
+++ src/year2020/mod.rs
@@ -9,6 +9,7 @@ mod day01;
 mod day02;
 mod day03;
 mod day04;
+mod day05;
 
 /// Solve a problem in the year 2020
 pub fn solve(day: Day, input: PathBuf) -> anyhow::Result<()> {
@@ -17,6 +18,7 @@ pub fn solve(day: Day, input: PathBuf) -> anyhow::Result<()> {
         2 => parse_and_solve::<day02::Day>(input)?,
         3 => parse_and_solve::<day03::Day>(input)?,
         4 => parse_and_solve::<day04::Day>(input)?,
+        5 => parse_and_solve::<day05::Day>(input)?,
         _ => return Err(anyhow::anyhow!("{} is not implemented", day)),
     }
     Ok(())
