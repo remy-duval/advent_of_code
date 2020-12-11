56fdccc4cd43daaba87354ecd5b23da5f15f2eb2
Remy Duval <remy.duval@macq.eu>
2020-12-11 14:25:19 +0100

    refactor: improve solution for year 2020 day 10 by using a sorted array instead of a set

--- src/year2020/day10.rs
+++ src/year2020/day10.rs
@@ -1,20 +1,17 @@
-use std::collections::HashSet;
 use std::str::FromStr;
 
-use itertools::Itertools;
-
+use crate::parse::LineSep;
 use crate::Problem;
 
 pub struct Day;
 
 impl Problem for Day {
-    type Input = Adapters;
-    type Err = anyhow::Error;
+    type Input = SortedAdapters;
+    type Err = std::convert::Infallible;
     const TITLE: &'static str = "Day 10: Adapter Array";
 
     fn solve(data: Self::Input) -> Result<(), Self::Err> {
-        let (ones, threes) = first_part(&data.0)
-            .ok_or_else(|| anyhow::anyhow!("Could not find the full adapter chain"))?;
+        let (ones, threes) = first_part(&data);
 
         println!(
             "The full adapter chain has {ones} (1V difference) * {threes} (3V difference) = {sum}",
@@ -25,110 +22,72 @@ impl Problem for Day {
 
         println!(
             "The number of arrangements of adapters for reaching the wanted value is: {total}",
-            total = second_part(&data.0)
+            total = second_part(data)
         );
 
         Ok(())
     }
 }
 
-pub struct Adapters(HashSet<usize>);
+/// Contains the adapters for the problems, already sorted and including the last one (max + 3)
+pub struct SortedAdapters(Vec<usize>);
 
-impl FromStr for Adapters {
+impl FromStr for SortedAdapters {
     type Err = std::num::ParseIntError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(Adapters(
-            s.lines().map(|line| line.trim().parse()).try_collect()?,
-        ))
+        let mut elements = s.parse::<LineSep<usize>>()?.data;
+        elements.sort_unstable();
+        Ok(SortedAdapters(elements))
     }
-}
-
-/// The maximum value of all the given adapters
-/// ### Arguments
-/// * `adapters` - All the adapters that are considered
-///
-/// ### Returns
-/// The maximum value of the adapters (or 0 if there are none)
-fn max(adapters: &HashSet<usize>) -> usize {
-    adapters.iter().copied().max().unwrap_or(0)
 }
 
 /// Find a chain of all the adapters that connect 0 to max + 3
 /// Then return the number of 1V differences and the number of 3V differences in it
 ///
 /// ### Arguments
-/// * `adapters` - All the adapters that are considered
+/// * `adapters` - All the adapters that are considered (must be sorted)
 ///
 /// ### Returns
 /// Maybe (ones, threes), the counts of 1V diff and 3V diff in the chain of adapters found
-fn first_part(adapters: &HashSet<usize>) -> Option<(u32, u32)> {
-    let all = adapters.len();
-    let maximum = max(adapters);
-    let mut stack = vec![(0, Chain::default())];
-    while let Some((next, chain)) = stack.pop() {
-        if next == maximum {
-            if chain.steps == all {
-                return Some((chain.ones, chain.threes + 1));
-            }
-        } else if adapters.contains(&next) || next == 0 {
-            stack.push((next + 3, chain.next(3)));
-            stack.push((next + 2, chain.next(2)));
-            stack.push((next + 1, chain.next(1)));
-        }
-    }
+fn first_part(adapters: &SortedAdapters) -> (usize, usize) {
+    let (ones, threes, _) = {
+        // The adapters are sorted, so this is already the full chain !
+        // The only thing that remains is to count the number of 1s and 3s differences
+        // Also the last adapter in the problem is MAX + 3V, so the 'threes' counter starts at 1
+        adapters
+            .0
+            .iter()
+            .copied()
+            .fold((0, 1, 0), |(ones, threes, current), next| {
+                let difference = next - current;
+                let ones = if difference == 1 { ones + 1 } else { ones };
+                let threes = if difference == 3 { threes + 1 } else { threes };
+                (ones, threes, next)
+            })
+    };
 
-    None
+    (ones, threes)
 }
 
 /// Find the possibilities for arranging adapters to get to the maximum value in V
 ///
 /// ### Arguments
-/// * `adapters` - All the adapters that are considered
+/// * `adapters` - All the adapters that are considered (must be sorted)
 ///
 /// ### Returns
 /// The number of arrangement of adapters that can chain to reach their maximum V value
-fn second_part(adapters: &HashSet<usize>) -> usize {
-    let maximum = max(adapters) + 1;
-    let mut first = 0; // The arrangements of V - 3
-    let mut second = 0; // The arrangements of V - 2
-    let mut third = 1; // The arrangements of V - 1 (The base of 0 V is 1 arrangement)
-    let mut possibilities = 0; // The arrangements of V
-    for voltage in 1..maximum {
-        if adapters.contains(&voltage) {
-            // If V is an adapter, its arrangements are the addition of the 3 previous ones
-            possibilities = third + second + first;
-        } else {
-            // If V is not an adapter, it can't have any arrangements
-            possibilities = 0;
+fn second_part(adapters: SortedAdapters) -> usize {
+    if let Some(maximum) = adapters.0.last() {
+        let mut memoized = vec![0; maximum + 1];
+        memoized[0] = 1;
+        for voltage in adapters.0 {
+            memoized[voltage] = memoized[voltage.saturating_sub(3)..voltage].iter().sum();
         }
-        first = second;
-        second = third;
-        third = possibilities;
-    }
 
-    possibilities
-}
-
-/// Hold the information about an adapter chain for the first part
-#[derive(Debug, Default)]
-struct Chain {
-    steps: usize,
-    ones: u32,
-    threes: u32,
-}
-
-impl Chain {
-    /// Get the chain for the given next step
-    /// If it is 1, increase the `one`
-    /// If it is 3, increase the `three`
-    /// In any case increase the `step`
-    fn next(&self, next: u8) -> Self {
-        Chain {
-            steps: self.steps + 1,
-            ones: self.ones + if next == 1 { 1 } else { 0 },
-            threes: self.threes + if next == 3 { 1 } else { 0 },
-        }
+        memoized.last().map_or(0, |last| *last)
+    } else {
+        0
     }
 }
 
@@ -142,43 +101,43 @@ mod tests {
 
     #[test]
     fn first_part_test_a() {
-        let adapters = Day::parse(A).unwrap().0;
-        let (ones, threes) = first_part(&adapters).unwrap();
+        let adapters = Day::parse(A).unwrap();
+        let (ones, threes) = first_part(&adapters);
         assert_eq!(7, ones);
         assert_eq!(5, threes);
     }
 
     #[test]
     fn first_part_test_b() {
-        let adapters = Day::parse(B).unwrap().0;
-        let (ones, threes) = first_part(&adapters).unwrap();
+        let adapters = Day::parse(B).unwrap();
+        let (ones, threes) = first_part(&adapters);
         assert_eq!(22, ones);
         assert_eq!(10, threes);
     }
 
     #[test]
     fn first_part_test_c() {
-        let adapters = Day::parse(C).unwrap().0;
-        let (ones, threes) = first_part(&adapters).unwrap();
+        let adapters = Day::parse(C).unwrap();
+        let (ones, threes) = first_part(&adapters);
         assert_eq!(66, ones);
         assert_eq!(39, threes);
     }
 
     #[test]
     fn second_part_test_a() {
-        let adapters = Day::parse(A).unwrap().0;
-        assert_eq!(8, second_part(&adapters));
+        let adapters = Day::parse(A).unwrap();
+        assert_eq!(8, second_part(adapters));
     }
 
     #[test]
     fn second_part_test_b() {
-        let adapters = Day::parse(B).unwrap().0;
-        assert_eq!(19_208, second_part(&adapters));
+        let adapters = Day::parse(B).unwrap();
+        assert_eq!(19_208, second_part(adapters));
     }
 
     #[test]
     fn second_part_test_c() {
-        let adapters = Day::parse(C).unwrap().0;
-        assert_eq!(2_644_613_988_352, second_part(&adapters))
+        let adapters = Day::parse(C).unwrap();
+        assert_eq!(2_644_613_988_352, second_part(adapters))
     }
 }
