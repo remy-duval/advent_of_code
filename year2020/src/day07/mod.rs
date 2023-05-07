f92d0d68701483b91c89b68ea019f16677b848d5
Remy DUVAL <remy_duval@laposte.net>
2023-05-08 00:44:03 +0200

    perf: improve day 07 year 2020

--- year2020/src/day07/mod.rs
+++ year2020/src/day07/mod.rs
@@ -1,98 +1,143 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 
-use commons::{Result, WrapErr};
+use commons::{ensure, Result, WrapErr};
 
 pub const TITLE: &str = "Day 7: Handy Haversacks";
 pub fn run(raw: String) -> Result<()> {
     let rules = parse(&raw)?;
-    println!("{} bags contains a {WANTED_BAG} bag", first_part(&rules));
-    println!("A {WANTED_BAG} will contain {} bags", second_part(&rules));
+    let containing_bags = first_part(&rules);
+    println!("{containing_bags} bags contains a {WANTED_BAG} bag");
+    let contained_bags = second_part(&rules);
+    println!("A {WANTED_BAG} will contain {contained_bags} bags");
     Ok(())
 }
 
 /// The wanted bag
 const WANTED_BAG: &str = "shiny gold";
-type Bag<'a> = &'a str;
-type BagRule<'a> = HashMap<Bag<'a>, u32>;
-type Rules<'a> = HashMap<Bag<'a>, BagRule<'a>>;
+
+struct Rules {
+    /// Each element is a bag content
+    by_index: Vec<Vec<BagContent>>,
+    /// The index of the wanted bag in the Vec
+    wanted: usize,
+}
+
+struct BagContent {
+    /// The index of the contained (u32 to use less memory)
+    contained: u32,
+    /// Number of times this bag is contained
+    count: u32,
+}
 
 fn parse(raw: &str) -> Result<Rules> {
-    raw.lines()
+    // Parse the descriptions of each bag content by name
+    let by_name: HashMap<&str, (usize, &str)> = raw
+        .lines()
         .map(|line| line.trim_end_matches('.'))
-        .map(|line| {
+        .enumerate()
+        .map(|(index, line)| {
             line.split_once("bags contain")
-                .wrap_err_with(|| format!("Missing element in the string for a rule {line}"))
-                .and_then(|(bag, rules)| match rules.trim() {
-                    "no other bags" => Ok((bag.trim(), HashMap::default())),
-                    rules => rules
-                        .split(',')
-                        .map(|rule| {
-                            let rule = rule.trim().trim_end_matches("bag").trim_end_matches("bags");
-                            let (number, bag) = rule.split_once(' ').wrap_err_with(|| {
-                                format!("Missing element in the string for a rule {rule}")
-                            })?;
-
-                            let number: u32 = number.trim().parse().wrap_err_with(|| {
-                                format!("Could not parse a number of bag in the rule {rule}")
-                            })?;
-
-                            Ok((bag.trim(), number))
-                        })
-                        .collect::<Result<_>>()
-                        .map(|rules| (bag.trim(), rules)),
-                })
+                .wrap_err_with(|| format!("missing elements in {line}"))
+                .map(|(bag, content)| (bag.trim(), (index, content)))
         })
-        .collect()
+        .collect::<Result<_>>()?;
+
+    ensure!(
+        by_name.len() < u32::MAX as usize,
+        "too many bags to fit in a 32-bits int"
+    );
+
+    // Convert all the names into indexes in a Vec for lookup efficiency
+    let mut by_index: Vec<Vec<BagContent>> = Vec::with_capacity(by_name.len());
+    (0..by_index.capacity()).for_each(|_| by_index.push(Vec::new()));
+    for (bag, (index, line)) in by_name.iter() {
+        by_index[*index] = match line.trim() {
+            "no other bags" => continue,
+            description => description
+                .split(',')
+                .map(|content| {
+                    let (num, bag) = content
+                        .trim()
+                        .trim_end_matches("bag")
+                        .trim_end_matches("bags")
+                        .split_once(' ')
+                        .wrap_err("missing contained bags syntax")?;
+
+                    let count = num
+                        .trim()
+                        .parse()
+                        .wrap_err_with(|| format!("number {num}"))?;
+
+                    let contained = by_name
+                        .get(bag.trim())
+                        .wrap_err_with(|| format!("missing the line describing {bag}"))
+                        .map(|t| t.0 as u32)?;
+
+                    Ok(BagContent { contained, count })
+                })
+                .collect::<Result<_>>()
+                .wrap_err_with(|| format!("in '{bag} bags contain {description}'"))?,
+        };
+    }
+
+    // Find the position of the wanted bag
+    let wanted = by_name
+        .get(WANTED_BAG)
+        .wrap_err_with(|| format!("missing target bag {WANTED_BAG} line"))
+        .map(|t| t.0)?;
+
+    Ok(Rules { by_index, wanted })
 }
 
 /// Find the number of bags that can contain the wanted one (recursively)
-fn first_part(all_rules: &Rules) -> usize {
-    let mut remaining = all_rules.iter().map(|(&k, v)| (k, v)).collect::<Vec<_>>();
-    let mut wanted = HashSet::from([WANTED_BAG]);
-    let mut next_wanted = Vec::new();
+fn first_part(rules: &Rules) -> usize {
+    let length = rules.by_index.len();
+    let mut bags: Vec<u32> = (0..(length as u32)).collect();
+    bags.swap(rules.wanted, length - 1);
+
+    let (mut remaining, mut wanted) = bags.split_at_mut(length - 1);
     while !wanted.is_empty() {
-        remaining.retain(|(bag, inner)| {
-            if inner.keys().any(|k| wanted.contains(*k)) {
-                next_wanted.push(*bag);
-                false
+        wanted.sort_unstable(); // Required for binary search
+        let mut start = 0;
+        let mut end = remaining.len();
+        while start < end {
+            let start_contains_wanted = rules.by_index[remaining[start] as usize]
+                .iter()
+                .any(|c| wanted.binary_search(&c.contained).is_ok());
+
+            if start_contains_wanted {
+                remaining.swap(start, end - 1);
+                end -= 1;
             } else {
-                true
+                start += 1;
             }
-        });
-        wanted.clear();
-        wanted.extend(next_wanted.drain(..));
+        }
+
+        (remaining, wanted) = remaining.split_at_mut(end);
     }
 
-    all_rules.len() - remaining.len()
+    length - remaining.len() - 1
 }
 
 /// Count the numbers of bag inside the wanted one
 fn second_part(all_rules: &Rules) -> u32 {
-    count_bags_inside(all_rules, WANTED_BAG, &mut HashMap::default())
+    let mut cache = vec![0; all_rules.by_index.len()];
+    count_bags_inside(all_rules, all_rules.wanted, &mut cache)
 }
 
-/// Count the number of bags inside one
-///
-/// ### Arguments
-/// * `all_rules` - The mapping between bags and the bags they should contain and their numbers
-/// * `bag` - The bag we want to count the inner values for
-/// * `already_counted` - A memoization already counted bags to improve performance
-///
-/// ### Returns
-/// The number of bags inside `bag`
-fn count_bags_inside<'a>(
-    all_rules: &Rules<'a>,
-    bag: Bag<'a>,
-    already_counted: &mut HashMap<Bag<'a>, u32>,
-) -> u32 {
+/// Count the number of bags inside the bag with the given index
+fn count_bags_inside(all_rules: &Rules, bag: usize, cache: &mut [u32]) -> u32 {
     let mut sum: u32 = 0;
-    for (inner, times) in all_rules.get(bag).into_iter().flatten() {
-        if let Some(count) = already_counted.get(*inner) {
-            sum += *count * times;
-        } else {
-            let count = count_bags_inside(all_rules, inner, already_counted) + 1;
-            already_counted.insert(inner, count);
-            sum += count * times;
+    for content in all_rules.by_index.get(bag).into_iter().flatten() {
+        let index = content.contained as usize;
+        if let Some(mut count) = cache.get(index).copied() {
+            // The cached value is always at least 1, 0 is not yet computed
+            if count == 0 {
+                count = count_bags_inside(all_rules, index, cache) + 1;
+                cache[index] = count;
+            }
+
+            sum += count * content.count;
         }
     }
     sum
